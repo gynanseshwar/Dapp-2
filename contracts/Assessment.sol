8990d91e48c71d8bf823aadf80637a923f93e9d9
gynanseshwar <148713548+gynanseshwar@users.noreply.github.com>
2023-12-16 15:39:18 +0530

    Update Assessment.sol

--- contracts/Assessment.sol
+++ contracts/Assessment.sol
@@ -1,14 +1,17 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-//import "hardhat/console.sol";
-
 contract Assessment {
     address payable public owner;
     uint256 public balance;
+    uint256 public fixedDepositBalance;
+    uint256 public fixedDepositDuration;
+    uint256 public fixedDepositStartTime;
 
     event Deposit(uint256 amount);
     event Withdraw(uint256 amount);
+    event FixedDeposit(uint256 amount, uint256 durationInYears);
+    event WithdrawFixedDeposit(uint256 amount);
 
     constructor(uint initBalance) payable {
         owner = payable(msg.sender);
@@ -21,21 +24,12 @@ contract Assessment {
 
     function deposit(uint256 _amount) public payable {
         uint _previousBalance = balance;
-
-        // make sure this is the owner
         require(msg.sender == owner, "You are not the owner of this account");
-
-        // perform transaction
         balance += _amount;
-
-        // assert transaction completed successfully
         assert(balance == _previousBalance + _amount);
-
-        // emit the event
         emit Deposit(_amount);
     }
 
-    // custom error
     error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
 
     function withdraw(uint256 _withdrawAmount) public {
@@ -47,14 +41,26 @@ contract Assessment {
                 withdrawAmount: _withdrawAmount
             });
         }
-
-        // withdraw the given amount
         balance -= _withdrawAmount;
-
-        // assert the balance is correct
         assert(balance == (_previousBalance - _withdrawAmount));
-
-        // emit the event
         emit Withdraw(_withdrawAmount);
+    }
+
+    function fixedDeposit(uint256 _amount, uint256 _durationInYears) public payable {
+        require(msg.sender == owner, "You are not the owner of this account");
+        require(_durationInYears >= 1, "Duration should be at least 1 year");
+        fixedDepositBalance += _amount;
+        fixedDepositDuration = _durationInYears;
+        fixedDepositStartTime = block.timestamp;
+        emit FixedDeposit(_amount, _durationInYears);
+    }
+
+    function withdrawFixedDeposit() public {
+        require(msg.sender == owner, "You are not the owner of this account");
+        require(block.timestamp >= fixedDepositStartTime + (fixedDepositDuration * 1 minutes), "Fixed deposit is not matured yet");
+        uint256 withdrawalAmount = fixedDepositBalance;
+        fixedDepositBalance = 0;
+        balance += withdrawalAmount;
+        emit WithdrawFixedDeposit(withdrawalAmount);
     }
 }
